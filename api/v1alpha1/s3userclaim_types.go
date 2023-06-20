d65edd991d922120283f97e2188cd754c2adc011
Parham Alvani <parham.alvani@gmail.com>
2023-06-20 07:00:33 +0330

    feat: add s3 user claim

---
+++ api/v1alpha1/s3userclaim_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// S3UserClaimSpec defines the desired state of S3UserClaim
+type S3UserClaimSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of S3UserClaim. Edit s3userclaim_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// S3UserClaimStatus defines the observed state of S3UserClaim
+type S3UserClaimStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// S3UserClaim is the Schema for the s3userclaims API
+type S3UserClaim struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   S3UserClaimSpec   `json:"spec,omitempty"`
+	Status S3UserClaimStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// S3UserClaimList contains a list of S3UserClaim
+type S3UserClaimList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []S3UserClaim `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&S3UserClaim{}, &S3UserClaimList{})
+}
