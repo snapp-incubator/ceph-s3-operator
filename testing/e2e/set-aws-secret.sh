cd9cdbc962cc2b17b86990853d10de94aeccb997
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2023-09-07 00:17:55 +0330

    add region to the e2e test

--- testing/e2e/set-aws-secret.sh
+++ testing/e2e/set-aws-secret.sh
@@ -3,6 +3,7 @@
 # Replace with your Kubernetes secret name and profile name
 SECRET_NAME="s3-sample-admin-secret"
 PROFILE_NAME="ceph-test"
+REGION="us-east-1"
 
 # Get the access key and secret key from Kubernetes secret
 ACCESS_KEY=$(kubectl get secret $SECRET_NAME -n s3-test -o jsonpath="{.data.accessKey}" | base64 --decode)
@@ -11,3 +12,4 @@ SECRET_ACCESS_KEY=$(kubectl get secret $SECRET_NAME -n s3-test -o jsonpath="{.da
 # Update the existing profile or create a new one
 aws configure set aws_access_key_id $ACCESS_KEY --profile $PROFILE_NAME
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY --profile $PROFILE_NAME
+aws configure set region $REGION --profile $PROFILE_NAME
