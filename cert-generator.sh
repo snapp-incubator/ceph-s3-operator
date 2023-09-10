311477ff9095d7cceb5e2dfc0960febbd3469b38
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2023-09-10 17:44:54 +0330

    add test step for userref update

---
+++ cert-generator.sh
@@ -0,0 +1,16 @@
+# run these commands inside your operator project directory
+
+# export the CAROOT env variable, used by the mkcert tool to generate CA and certs
+export CAROOT=/tmp/k8s-webhook-server/serving-certs
+
+# then, install a new CA
+# the following command will create 2 files rootCA.pem and rootCA-key.pem
+mkcert -install
+
+# then, generate SSL certificates
+# here, we're creating certificates valid for both "host.docker.internal" for MacOS and "172.17.0.1" for Linux
+# and put them inside the certs/tls.crt and certs/tls.key files (by default the operator/webhook will look for certificates with this naming convention)
+mkcert -cert-file=$CAROOT/tls.crt -key-file=$CAROOT/tls.key host.minikube.internal 192.168.64.1 host.docker.internal 172.17.0.1
+CA_BUNDLE=$(cat $CAROOT/rootCA.pem | base64 -w 0)
+
+sed -i "s|CA_BUNDLE_PLACEHOLDER|$CA_BUNDLE|" ./config/local/webhook-clientconfig-patch.yaml
